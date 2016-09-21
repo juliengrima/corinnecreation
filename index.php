42f3f7c94a7a62ea3500827b7cf96844656f04d0
julien_grima <gogrinformatique@gmail.com>
2016-09-21 09:57:26 +0200

    changement des images

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- PARALLAX -->
     <div class="parallax-container">
         <div class="parallax">
-            <img id="image" src="images/secret_garden.jpg" data-img-width="1600" data-img-height="1064" data-diff="100">
+            <img id="image" src="images/baroque5.jpg" data-img-width="1600" data-img-height="1064" data-diff="100">
         </div>
         
         <!-- REPERES DE COLONNES POUR IMPLEMENTATION -->
@@ -32,50 +32,33 @@
         </div>
         </div>
         
-        <div class="container center">
+        <div class="container">
             <div class="row">
                 <div class="col s12 m6 l3">
                     <div class="card">
                         <div class="card-image waves-effect waves-block waves-light">
-                            <img class="activator" alt="livraison services" src="images/galets.jpg">
+                            <img class="activator" alt="livraison services" src="images/collection.jpg">
                         </div>
                         <div class="card-content" id="cont"> 
-                            <span class="card-title activator grey-text text-darken-4">MINERAL<i class="material-icons right">more_vert</i></span>
+                            <span class="card-title activator grey-text text-darken-4">Events<i class="material-icons right">more_vert</i></span>
                             <p><a href="mineral.php" id="lien">Aller voir</a></p>
                         </div>
                         <div class="card-reveal"> 
-                            <span class="card-title grey-text text-darken-4">MINERAL<i class="material-icons right">close</i></span>
+                            <span class="card-title grey-text text-darken-4">Events<i class="material-icons right">close</i></span>
                             <br>
                             <br>
                             <div class="marge">
                                 <table>
                                     <tr>
-                                        <li><a href="" id="lien">Ardoises</a></li>
+                                        <td>Bonjour,<br>
+                                            Dans cette rubrique,<br>
+                                            vous trouverez tous les<br>
+                                            évènements dans lesquels<br>
+                                            je serais présente.</td>
                                     </tr>
                                     <tr>
-                                        <li><a href="" id="lien">Briques</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Décoratifs</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Galets</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Gros volumes</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">OPUS</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Pouzzolane</a></li>
-                                    </tr>
-                                    <tr>
-                                        <lI><a href="" id="lien">Sable</a></lI>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Schiste</a></li>
-                                    </tr>   
+                                        <td><a href="">Trouver un évènement</a></td>
+                                    </tr> 
                                 </table>
                             </div>
                         </div>
@@ -84,39 +67,29 @@
                 <div class="col s12 m6 l3">
                     <div class="card">
                         <div class="card-image waves-effect waves-block waves-light">
-                            <img class="activator" alt="livraison services" src="images/chanvre.jpg">
+                            <img class="activator" alt="livraison services" src="images/atelier3.jpg">
                         </div>
                         <div class="card-content" id="cont"> 
-                            <span class="card-title activator grey-text text-darken-4">VEGETAL<i class="material-icons right">more_vert</i></span>
+                            <span class="card-title activator grey-text text-darken-4">Atelier<i class="material-icons right">more_vert</i></span>
                             <p><a href="#" id="lien">Aller voir</a></p>
                         </div>
                         <div class="card-reveal"> 
-                            <span class="card-title grey-text text-darken-4">VEGETAL<i class="material-icons right">close</i></span>
+                            <span class="card-title grey-text text-darken-4">Atelier<i class="material-icons right">close</i></span>
                             <br>
                             <br>
                             <div class="marge">
                                 <table>
                                     <tr>
-                                        <li><a href="" id="lien">Chanvre</a></li>
+                                        <td>
+                                            Voici mon atelier,<br>
+                                            là ou la magie opère<br>
+                                            et d'ou sortent de mon<br>
+                                            imagination toutes les créations.
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <li><a href="" id="lien">Ecorce de pin</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Noyaux de fruit</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Plaquette Peuplier</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Terre de Bruyère</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Terre végétale</a></li>
-                                    </tr>
-                                    <tr>
-                                        <li><a href="" id="lien">Terreau Ecolabel</a></li>
-                                    </tr>   
+                                        <td><a href="">Visiter l'atelier</a></td>
+                                    </tr> 
                                 </table>
                             </div>
                         </div>
@@ -125,36 +98,29 @@
             <div class="col s12 m6 l3">
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" alt="livraison services" src="images/gazon.jpg">
+                        <img class="activator" alt="livraison services" src="images/mpas.jpg">
                     </div>
                     <div class="card-content" id="cont"> 
-                        <span class="card-title activator grey-text text-darken-4">ACCESSOIRES<i class="material-icons right">more_vert</i></span>
+                        <span class="card-title activator grey-text text-darken-4">Me trouver<i class="material-icons right">more_vert</i></span>
                         <p><a href="#" id="lien">Aller voir</a></p>
                     </div>
                     <div class="card-reveal"> 
-                        <span class="card-title grey-text text-darken-4">ACCESSOIRES<i class="material-icons right">close</i></span>
+                        <span class="card-title grey-text text-darken-4">Me trouver<i class="material-icons right">close</i></span>
                         <br>
                         <br>
                         <div class="marge">
                             <table>
                                 <tr>
-                                    <li><a href="" id="lien">Bordures</a></li>
-                                </tr>
-                                <tr>
-                                    <li><a href="" id="lien">Dalles alvéolées</a></li>
-                                </tr>
-                                <tr>
-                                    <li><a href="" id="lien">Engrais</a></li>
-                                </tr>
-                                <tr>
-                                    <li><a href="" id="lien">Gazon</a></li>
-                                </tr>
-                                <tr>
-                                    <li><a href="" id="lien">Gazon de placage</a></li>
-                                </tr>
-                                <tr>
-                                    <li><a href="" id="lien">Traverse</a></li>
-                                </tr>  
+                                    <td>
+                                        Vous qui cherchez à<br>
+                                        trouver mon repère,<br>
+                                        c'est ici que vous devez<br>
+                                        aller.
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><a href="">Me trouver</a></td>
+                                    </tr> 
                             </table>
                         </div>
                     </div>
@@ -163,14 +129,17 @@
             <div class="col s12 m6 l3">
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" alt="livraison services" src="images/galets.jpg">
+                        <img class="activator" alt="livraison services" src="images/facebook.jpg" id="taille">
+                    </div>
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" alt="livraison services" src="images/instagram.png" id="taille">
                     </div>
                     <div class="card-content" id="cont"> 
-                        <span class="card-title activator grey-text text-darken-4">MINERAL<i class="material-icons right">more_vert</i></span>
+                        <span class="card-title activator grey-text text-darken-4">Mes liens<i class="material-icons right">more_vert</i></span>
                         <p><a href="mineral.php" id="lien">Aller voir</a></p>
                     </div>
                     <div class="card-reveal"> 
-                        <span class="card-title grey-text text-darken-4">MINERAL<i class="material-icons right">close</i></span>
+                        <span class="card-title grey-text text-darken-4">Mes liens<i class="material-icons right">close</i></span>
                         <br>
                         <br>
                         <div class="marge">
